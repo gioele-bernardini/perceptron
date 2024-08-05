1d2c0a657a3a7ee0c59bd3e35c2abbfc8671a765
gioele-bernardini <gioele.g@outlook.com>
2024-08-05 17:58:33 +0200

    Add: Weight adjustment example added.

--- examples/weight_adjustment.c
+++ examples/weight_adjustment.c
@@ -17,31 +17,31 @@ float rand_float(void) {
   return (float) rand() / (float) RAND_MAX;
 }
 
+float loss_function(float weight) {
+  float result = 0.0f;
+
+  for (size_t i = 0; i < train_count; i++) {
+    float x = train[i][0];
+    float y = x * weight;
+    float d = y - train[i][1];
+
+    result += d * d;
+  }
+
+  result /= train_count;
+  return result;
+}
+
 int main() {
   // srand(time(0));
   srand(42);
 
   // y = x*w;
   float w = rand_float() * 10.0f;
-
   float eps = 1e-3;
 
-  float result = 0;
-
-  for (size_t i = 0; i < train_count; i++) {
-    float x = train[i][0];
-    float y = x * w;
-
-    // Define a way to measure the difference between the actual value and the expected one
-    float d = y - train[i][1];
-    result += d * d;
-
-    printf("Actual: %f, expected: %f\n", y, train[i][1]);
-  }
-
-  result /= train_count;
-
-  printf("%f\n", result);
+  printf("%f\n", loss_function(w));
+  printf("%f\n", loss_function(w + eps));
 
   return 0;
 }
